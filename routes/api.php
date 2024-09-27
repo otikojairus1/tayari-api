27f8efbeb93d6ff7836d60d94b13a72ba6785945
otikojairus1 <otikojairus@gmail.com>
2024-09-27 10:45:53 +0300

    chore:added kids controllrt

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/verify', [UserController::class, 'verify']);
+Route::post('/add/kids', [UserController::class, 'add_kids']);
+Route::put('/modify/kids/{id}', [UserController::class, 'update_kids']);
+Route::get('/get/kids/{id}', [UserController::class, 'get_kids']);
+Route::put('/modify/parents/{id}', [UserController::class, 'modify_parents']);
+Route::post('/add-kid-homework-progress', [UserController::class, 'add_kid_homework_progress']);
