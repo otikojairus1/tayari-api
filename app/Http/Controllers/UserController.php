d3c51af3f9b581005cb39ea6ce095ffe58705d14
otikojairus1 <otikojairus@gmail.com>
2024-09-27 10:45:16 +0300

    chore:added kids controllrt

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\SendOtpMail;
+use App\Models\Kid;
+use App\Models\ParentModel;
+use App\Models\Score;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . ParentModel::class],
+            'password' => ['required'],
+            'phone' => ['required'],
+            'otp' => ['required'],
+            'country' => ['required'],
+        ]);
+        if ($validator->fails()) {
+            $response = $validator->errors();
+            return response()->json(['success' => false, 'data' => $response]);
+        }
+
+        $user = ParentModel::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'phone' => $request->phone,
+            'otp' => mt_rand(1000, 9999),
+            'email' => $request->email,
+            'country' => $request->country,
+            'password' => Hash::make($request->string('password')),
+        ]);
+        Mail::to($user->email)->send(new SendOtpMail($user));
+        return response()->json(['success' => true, 'data' => $user]);
+    }
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required'],
+        ]);
+        if ($validator->fails()) {
+            $response = $validator->errors();
+            return response()->json(['success' => false, 'data' => $response]);
+        }
+
+        $user = ParentModel::where('email', $request->email)->first();
+
+        if (!$user) {
+            return response()->json(['success' => false, 'data' => 'wrong USER ACCOUNT used']);
+
+        }
+        if (Hash::check($request->password, $user->password)) {
+            return response()->json(['success' => true, 'data' => $user]);
+        }
+
+        return response()->json(['success' => false, 'data' => 'wrong password used']);
+
+    }
+
+    public function add_kids(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'parent_id' => ['required'],
+            'child_name' => ['required', 'string', 'max:255'],
+            'child_dob' => ['required'],
+            'user_level' => ['required'],
+            'current_day' => ['required'],
+            'current_unit' => ['required'],
+            'content_progress' => ['required'],
+            'streaks' => ['required'],
+        ]);
+        if ($validator->fails()) {
+            $response = $validator->errors();
+            return response()->json(['success' => true, 'data' => $response]);
+        }
+
+        $user = Kid::create([
+            'parent_id' => $request->parent_id,
+            'child_name' => $request->child_name,
+            'child_dob' => $request->child_dob,
+            'user_level' => $request->user_level,
+            'current_day' => $request->current_day,
+            'current_unit' => $request->current_unit,
+            'content_progress' => $request->content_progress,
+            'streaks' => $request->streaks
+        ]);
+        return response()->json(['success' => true, 'data' => $user]);
+
+
+    }
+
+
+    public function update_kids(Request $request, $id)
+    {
+        $kid = Kid::find($id);
+        $kid->update($request->all());
+        return response()->json(['success' => true, 'data' => $kid]);
+
+    }
+
+
+    public function modify_parents(Request $request, $id)
+    {
+
+        $parent = ParentModel::find($id);
+        if ($parent) {
+            $parent->update($request->all());
+        return response()->json(['success' => true, 'data' => $parent]);
+
+        }
+        return response()->json(['success' => false, 'data' => 'no parent matches the details']);
+    }
+
+
+    public function verify(Request $request){
+        $validator = Validator::make($request->all(), [
+            'otp' => ['required'],
+            'id' => ['required'],
+        ]);
+        if ($validator->fails()) {
+            $response = $validator->errors();
+            return response()->json(['success' => true, 'data' => $response]);
+        }
+        $user = ParentModel::where('otp',$request->otp)->where('id',$request->id)->first();
+        if(!$user){
+            return response()->json(['success' => false, 'data' => 'invalid code']);
+        }
+        $user->otp = 'VERIFIED';
+        $user->save();
+        return response()->json(['success' => true, 'data' => 'verified']);
+    }
+
+    public function get_kids(Request $request, $id)
+    {
+        $kid = Kid::where('parent_id', $id)->get();
+        return response()->json(['success' => true, 'data' => $kid]);
+
+    }
+
+
+    public function add_kid_homework_progress(Request $request){
+        $validator = Validator::make($request->all(), [
+            'kid_id' => ['required'],
+            'level' => ['required'],
+            'day' => ['required'],
+            'unit' => ['required'],
+            'homework_score' => ['required'],
+        ]);
+        if ($validator->fails()) {
+            $response = $validator->errors();
+            return response()->json(['success' => true, 'data' => $response]);
+        }
+        $score = Score::updateOrCreate(
+            [
+                'kid_id' => $request->kid_id,
+                'level' => $request->level,
+                'day' => $request->day,
+                'unit' => $request->unit,
+            ],
+            [
+                'homework_score' => $request->homework_score,
+            ]
+        );
+        return response()->json(['success' => true, 'data' => $score]);        
+    }
+}
---
+++ app/Mail/SendOtpMail.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Address;
+
+class SendOtpMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        public $details 
+    ) {
+        $this->details = $details;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(            
+            from: new Address('jairus@jacquelinetsuma.com', 'TAYARI.LIVE'),
+            subject: '[TAYARI.LIVE] Please verify your Account',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.otp',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
